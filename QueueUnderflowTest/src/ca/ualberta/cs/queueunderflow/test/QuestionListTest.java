e584b9dd196a86fa3199e6a40bf66d3a981ee7c4
paul80 <paulnhan26@gmail.com>
2014-10-12 22:17:33 -0600

    Use cases 1-10 Junit implemented

--- QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/QuestionListTest.java
+++ QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/QuestionListTest.java
@@ -171,6 +171,216 @@ public class QuestionListTest extends TestCase {
 		
 	}
 	
+	//Use CASE 5: Incorporates user stories 5 and 7
+	
+	public void testAddAnswer() {
+		
+		QuestionList questionList= new QuestionList();
+		String questionName= "A question?";
+		String author= "Me";
+		Question questionTest= new Question(questionName,author);
+		questionList.add(questionTest);
+		int question_index= questionList.questionIndex(questionTest);
+		
+		Question sameQuestion= questionList.get(question_index);
+		//Exception: Where the answer entered is whitespaces only
+		String emptyAnswer= "          ";
+		String empty= emptyAnswer.trim();
+		String result;
+		if (empty.length()==0) {
+			result="Reenter an answer";
+		}
+		else {
+			result=null;
+		}
+		assertTrue("emptyAnswer is empty", result!=null);
+		
+		//Adding an answer
+		String answerName= "An answer";
+		String authorName= "You";
+		Answer testAnswer= new Answer(answerName,authorName);
+		sameQuestion.addAnswer(testAnswer);
+		questionList.set(question_index, sameQuestion);
+		assertTrue("Answer List isn't empty",questionList.get(question_index).getAnswerListSize()==1 ) ;
+		
+		//Exception: Where there is no online connectivity
+		assertFalse("No network connectivity, push online later.",questionList.pushOnline());
+	}
+	
+	//Use CASE 6 (incorporates user story 6)
+	public void testAddReply() {
+		QuestionList questionList= new QuestionList();
+		String questionName= "A question?";
+
+		String author= "A author";
+		
+		//Add question_replies
+		Question questionTest= new Question(questionName,author);
+		Reply question_reply=new Reply("What are you talking about", "Walter");
+		questionTest.addQuestionReply(question_reply);
+		assertTrue("Reply list isnt empty", questionTest.getSizeReplies()==1);
+		
+		//Add a answer and reply to the answer
+		String author2="Author 2";
+		String answerName= "An answer";
+		Answer testAnswer= new Answer(answerName,author2);
+		Reply answer_reply= new Reply("What are you asking", "Jason");
+		testAnswer.addReply(answer_reply);
+		assertTrue("Reply list isn't empty", testAnswer.getSizeReplies()==1);
+		
+		questionTest.addAnswer(testAnswer);
+		questionList.add(questionTest);
+		
+		//Exception: Where there is no online connectivity
+		assertFalse("No network connectivity, push online later.",questionList.pushOnline());
+	
+	}
+	
+	//Use CASE 7(incorporates user story 7)
+	public void testAddPhotoFromCamera() {
+		String questionName= "How does this work?";
+		String author= "Me";
+		Question questionTest= new Question(questionName,author);
+		Picture pic= new Picture(10);
+		questionTest.setPicture(pic);
+		
+		assertTrue("Question has a picture", questionTest.getPicture()!=null);
+		
+		String answerName= "An answer";
+		String authorName= "You";
+		Answer testAnswer= new Answer(answerName,authorName);
+		
+		testAnswer.setPicture(pic);
+		assertTrue("Answer has a picture", testAnswer.getPicture()!=null);
+		
+		//Exception: Photo is larger than 64 kb
+		Picture picture2= new Picture(65);
+		assertFalse("image is too large, select another photo", picture2.getSize()<64);
+	}
+	
+	//Use CASE 8 (incorporates user story 7 and 8)
+	public void testAddPhotoFromGallery() {
+		String questionName= "How does this work?";
+		String author= "Me";
+		Question questionTest= new Question(questionName,author);
+		Picture pic= new Picture(10);
+		questionTest.setPicture(pic);
+		
+		assertTrue("Question has a picture", questionTest.getPicture()!=null);
+		
+		String answerName= "An answer";
+		String authorName= "You";
+		Answer testAnswer= new Answer(answerName,authorName);
+		
+		testAnswer.setPicture(pic);
+		assertTrue("Answer has a picture", testAnswer.getPicture()!=null);
+		
+		//Exception: Photo is larger than 64 kb
+		Picture picture2= new Picture(65);
+		assertFalse("image is too large, select another photo", picture2.getSize()<64);
+	}
+	
+	//Use CASE 9 (incorporates user story 8)
+	public void testPhotoSizeLimit() {
+		String questionName= "How does this work?";
+		String author= "Me";
+		Question questionTest= new Question(questionName,author);
+		Picture pic= new Picture(65);
+		assertFalse("image is too large, select another photo", pic.getSize()<64);
+
+		//Exception: Photo is smaller than 64kb, no complaints
+		
+		Picture picture2= new Picture(61);
+		assertTrue("No complaints about picture size", picture2.getSize()<=64);
+		questionTest.setPicture(picture2);
+	}
+	
+	//Use CASE 10 (incorporates user stories 9 and 10)
+	public void testSortQuestionBy() {
+		String questionName= "Oldest question";
+		String author= "long time ago";
+		Question question1= new Question(questionName,author);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(question1.getDate());
+		//Make the date 5 seconds behind current date
+		cal.add(Calendar.SECOND,-5);
+		Date question1date = cal.getTime();
+		question1.setDate(question1date);
+		
+		String questionName2= "Middle date question";
+		String author2= "middle";
+		Question question2= new Question(questionName2,author2);
+		Calendar cal2 = Calendar.getInstance();  
+		cal2.setTime(question2.getDate());
+		//Make the date 3 seconds behind current date
+		cal2.add(Calendar.SECOND,-3);
+		Date question2date = cal2.getTime();
+		question2.setDate(question2date);
+		
+		String questionName3= "Freshest question";
+		String author3= "Now";
+		Question question3= new Question(questionName3,author3);
+		
+		//Set the upvotes each question has
+		question1.setUpvotes(1);
+		question2.setUpvotes(10);
+		question3.setUpvotes(5);
+		
+		QuestionList questionList = new QuestionList();
+		questionList.add(question2);
+		questionList.add(question1);
+		questionList.add(question3);
+		
+		ArrayList<Question> mostRecentList = new ArrayList<Question>();
+		mostRecentList.add(question3);
+		mostRecentList.add(question2);
+		mostRecentList.add(question1);
+		
+		ArrayList<Question> leastRecentList = new ArrayList<Question>();
+		leastRecentList.add(question1);
+		leastRecentList.add(question2);
+		leastRecentList.add(question3);
+		
+		
+		//Checking if the sorting methods work
+		ArrayList<Question> questionList3 = questionList.sortBy("most recent");
+		assertTrue("Questionlist3 1st slot is question3", questionList3.get(0).equals(question3));
+		assertTrue("Questionlist3 2nd slot is question2", questionList3.get(1).equals(question2));
+		assertTrue("Questionlist3 3rd slot ins question1", questionList3.get(2).equals(question1));
+		assertEquals("questionList3 is sorted by date, most recent to least recent", questionList3, mostRecentList);
+		
+		ArrayList<Question> questionList4 = questionList.sortBy("least recent");
+		assertTrue("Questionlist4 1st slot is question1", questionList4.get(0).equals(question1));
+		assertTrue("Questionlist4 2nd slot is question2", questionList4.get(1).equals(question2));
+		assertTrue("Questionlist4 3rd slot ins question3", questionList4.get(2).equals(question3));
+		assertEquals("questionList4 is sorted by date, least recent to most recent", questionList4, leastRecentList);
+		
+		ArrayList<Question> mostUpvotesList = new ArrayList<Question>();
+		mostUpvotesList.add(question2);
+		mostUpvotesList.add(question3);
+		mostUpvotesList.add(question1);
+		
+		ArrayList<Question> leastUpvotesList = new ArrayList<Question>();
+		leastUpvotesList.add(question1);
+		leastUpvotesList.add(question3);
+		leastUpvotesList.add(question2);
+		
+		ArrayList<Question> questionList5 = questionList.sortBy("most upvotes");
+		assertEquals("questionList1 is sorted by most upvotes to least", mostUpvotesList, questionList5);
+		
+		ArrayList<Question> questionList6 = questionList.sortBy("least upvotes");
+		assertEquals("questionList2 is sorted by least upvotes to most", leastUpvotesList, questionList6);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//*********************Below are the user stories in implementation, not actual use cases****************
 	
 	
