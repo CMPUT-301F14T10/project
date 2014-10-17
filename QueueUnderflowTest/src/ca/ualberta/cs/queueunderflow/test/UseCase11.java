c74ffdb3550c9ba597e893f71dcd9faf54041289
Xin Yi Wang <wxy604@hotmail.com>
2014-10-17 00:37:17 -0600

    edited usecases 10,11 and added test case 10,11

---
+++ QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/UseCase11.java
@@ -0,0 +1,40 @@
+package ca.ualberta.cs.queueunderflow.test;
+
+import ca.ualberta.cs.queueunderflow.Answer;
+import ca.ualberta.cs.queueunderflow.Question;
+import ca.ualberta.cs.queueunderflow.User;
+import junit.framework.TestCase;
+
+public class UseCase11 extends TestCase {
+	
+	public void testUpvote() {
+		User user=new User();
+		user.setUserName("A user");
+		
+		String userName= user.getUserName();
+		String questionName= "A question?";
+		String author= userName;
+		
+		//Add question_replies
+		Question questionTest= new Question(questionName, author);
+		questionTest.upvoteQuestion();
+		user.addUpvotedQuestion(questionTest);
+		assertTrue("Question upvoted by 1", questionTest.getUpvotes()==1);
+		
+		//Adding an answer
+
+		String author2="Author 2";
+		String answerName= "An answer";
+
+		
+		Answer testAnswer= new Answer(answerName,author2);
+		testAnswer.upvoteAnswer();
+		user.addUpvotedAnswer(testAnswer);
+		assertTrue("Answer upvoted by 1", testAnswer.getUpvotes()==1);
+		
+		//Exception: User already upvoted question/answer
+		assertTrue("Question already upvoted", user.alreadyUpvotedQuestion(questionTest));
+		assertTrue("Answer already upvoted", user.alreadyUpvotedAnswer(testAnswer));
+	}
+	
+}
