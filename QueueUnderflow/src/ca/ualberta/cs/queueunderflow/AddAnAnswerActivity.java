89901d96559f43338a8c0cffda054194c2a960dc
paul80 <paulnhan26@gmail.com>
2014-10-24 23:16:10 -0600

    Finished add an answer activity; made it so the question being answered appears and the previous answers appear in the expandable listview

--- QueueUnderflow/src/ca/ualberta/cs/queueunderflow/AddAnAnswerActivity.java
+++ QueueUnderflow/src/ca/ualberta/cs/queueunderflow/AddAnAnswerActivity.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter.AuthorityEntry;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddAnAnswerActivity extends Activity
 {
+	public static final int HOME_SCREEN_FRAGMENT = 1;
+	public static final int FAVORITES_FRAGMENT = 2;
+	public static final int READING_LIST_FRAGMENT = 3;
+	public static final int MY_QUESTIONS_FRAGMENT = 4;
+	AnswerListAdapter adapter;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -26,7 +36,48 @@ public class AddAnAnswerActivity extends Activity
 		Button addBtn = (Button) findViewById(R.id.AddAnAnswerButton);
 		
 		Intent intent = getIntent();
-		final int position = intent.getIntExtra("question_position", -1); 				// -1 is the default value if nothing was retrieved
+		final int position = intent.getIntExtra("question_position", -1); 	// -1 is the default value if nothing was retrieved
+		
+		QuestionList questionList=ListHandler.getMasterQList();
+		// Get the question selected
+		final Question question = questionList.get(position);
+		
+		
+		// Set up the display to display question and expandable listview of answers
+		TextView questionDisplay = (TextView) findViewById(R.id.questionTextView);
+		questionDisplay.setText(question.getQuestion());
+		
+		TextView authorDisplay = (TextView) findViewById(R.id.authorTextView);
+		authorDisplay.setText(questionList.get(0).getAuthor());
+		
+		TextView dateDisplay = (TextView) findViewById(R.id.dateTextView);
+		dateDisplay.setText(DateFormat.format("MMM dd, yyyy", question.getDate()));
+		
+		final TextView upvoteDisplay = (TextView) findViewById(R.id.upvoteDisplay);
+		upvoteDisplay.setText(Integer.toString(question.getUpvotes()));
+		
+		ImageButton upvoteBtn = (ImageButton) findViewById(R.id.upvoteBtn);
+		upvoteBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				question.upvoteQuestion();
+				upvoteDisplay.setText(Integer.toString(question.getUpvotes()));
+			}
+		});
+		
+		if (question.hasPicture() == true) {
+			ImageButton hasPictureIcon = (ImageButton) findViewById(R.id.hasPictureIcon);
+			hasPictureIcon.setVisibility(0);
+		}
+		
+        ExpandableListView answersExpListView = (ExpandableListView) findViewById(R.id.answersExpListView);
+        adapter = new AnswerListAdapter(this, R.layout.list_item_answer, R.layout.exp_list_item_reply, question.getAnswerList().getAnswerList());
+        answersExpListView.setAdapter(adapter);
+        
+        // This hides that expand arrow on the answer item
+        answersExpListView.setGroupIndicator(null);
+		
 		
 		addBtn.setOnClickListener(new OnClickListener()
 		{
