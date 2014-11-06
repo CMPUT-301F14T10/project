d51bb998042ad24a6699466bc7e8753d14ccd9e8
genevag <geneva@ualberta.ca>
2014-11-05 22:06:50 -0700

    added ui test for use case 3
    - it passes when run individually but I'm not sure why it has an error
    when running the entire package

--- QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/UseCase3.java
+++ QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/UseCase3.java
@@ -1,16 +1,81 @@
 package ca.ualberta.cs.queueunderflow.test;
 
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import ca.ualberta.cs.queueunderflow.ListHandler;
+import ca.ualberta.cs.queueunderflow.R;
+import ca.ualberta.cs.queueunderflow.adapters.AnswerListAdapter;
+import ca.ualberta.cs.queueunderflow.adapters.SingleQuestionAdapter;
 import ca.ualberta.cs.queueunderflow.models.Answer;
 import ca.ualberta.cs.queueunderflow.models.AnswerList;
 import ca.ualberta.cs.queueunderflow.models.Question;
 import ca.ualberta.cs.queueunderflow.models.QuestionList;
 import ca.ualberta.cs.queueunderflow.models.Reply;
+import ca.ualberta.cs.queueunderflow.views.QAViewActivity;
 import junit.framework.TestCase;
 
-
-public class UseCase3 extends TestCase
-{
-	//Use CASE 3 (incorporates user story 3)
+//Use CASE 3 (incorporates user story 3)
+public class UseCase3 extends ActivityInstrumentationTestCase2<QAViewActivity> {
+	
+	public UseCase3() {
+		super(QAViewActivity.class);
+	}
+	
+	public void testReplyDisplay() {
+		ListHandler.getMasterQList().getQuestionList().clear();
+		
+		Question question = new Question("Another Question", "Me");
+		Answer answer1 = new Answer("Answer #1", "You");
+		Answer answer2 = new Answer("Answer #2", "Him");
+		Reply aReply = new Reply("Answer Reply", "Her");
+		Reply qReply = new Reply("Question Reply", "Them");
+		answer1.addReply(aReply);
+		question.addQuestionReply(qReply);
+		question.addAnswer(answer1);
+		question.addAnswer(answer2);
+		ListHandler.getMasterQList().add(question);
+		
+		// Start the activity
+		Intent intent = new Intent();
+		intent.putExtra("fromFragment", QAViewActivity.HOME_SCREEN_FRAGMENT);
+		intent.putExtra("position", 0);
+		setActivityIntent(intent);
+		QAViewActivity activity = (QAViewActivity) getActivity();
+		
+		// Check question reply display
+		ExpandableListView qListView = (ExpandableListView) activity.findViewById(R.id.questionExpListView);
+		SingleQuestionAdapter singleQAdapter = (SingleQuestionAdapter) qListView.getExpandableListAdapter();
+		
+		assertEquals(question, singleQAdapter.getGroup(0));
+		assertEquals(1, singleQAdapter.getChildrenCount(0));
+		
+		View replyView = singleQAdapter.getChildView(0, 0, true, null, null);
+		TextView replyText = (TextView) replyView.findViewById(R.id.replyTextView);
+		TextView authorUsername = (TextView) replyView.findViewById(R.id.authorTextView);
+		assertEquals("Question Reply", replyText.getText().toString());
+		assertEquals("Them", authorUsername.getText().toString());
+		
+		
+		// Check answer reply display
+		ExpandableListView aListView = (ExpandableListView) activity.findViewById(R.id.answersExpListView);
+		AnswerListAdapter aAdapter = (AnswerListAdapter) aListView.getExpandableListAdapter();
+		
+		assertEquals(answer1, aAdapter.getGroup(1));
+		assertEquals(1, aAdapter.getChildrenCount(1));
+		
+		View replyView1 = aAdapter.getChildView(1, 0, true, null, null);
+		TextView replyText1 = (TextView) replyView1.findViewById(R.id.replyTextView);
+		TextView authorUsername1 = (TextView) replyView1.findViewById(R.id.authorTextView);
+		assertEquals("Answer Reply", replyText1.getText().toString());
+		assertEquals("Her", authorUsername1.getText().toString());
+		
+		activity.finish();
+	}
+	
+	
 	public void testViewReplies() {
 		String questionName= "A question?";
 		String author="A author";
