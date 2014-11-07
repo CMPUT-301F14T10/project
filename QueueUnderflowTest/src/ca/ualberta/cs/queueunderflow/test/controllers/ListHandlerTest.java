36e650f1a2c568e73d19e3c5bc59e5825ee1ae4c
pnhan <pnhan@ualberta.ca>
2014-11-07 13:19:04 -0700

    reorganized some test cases and put/cloned them into other packages

--- QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/controllers/ListHandlerTest.java
+++ QueueUnderflowTest/src/ca/ualberta/cs/queueunderflow/test/controllers/ListHandlerTest.java
@@ -1,4 +1,4 @@
-package ca.ualberta.cs.queueunderflow.test;
+package ca.ualberta.cs.queueunderflow.test.controllers;
 
 import ca.ualberta.cs.queueunderflow.ListHandler;
 import ca.ualberta.cs.queueunderflow.models.Question;
