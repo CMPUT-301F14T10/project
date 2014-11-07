e9ddd0709b01f061853c1f1bd4a7b6c596eb944d
genevag <geneva@ualberta.ca>
2014-11-06 22:16:07 -0700

    added ui test for use case 15 and 16

--- QueueUnderflow/gen/ca/ualberta/cs/queueunderflow/BuildConfig.java
+++ QueueUnderflow/gen/ca/ualberta/cs/queueunderflow/BuildConfig.java
@@ -1,12 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package ca.ualberta.cs.queueunderflow;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class BuildConfig.
- */
 public final class BuildConfig {
-    
-    /** The Constant DEBUG. */
     public final static boolean DEBUG = true;
 }
