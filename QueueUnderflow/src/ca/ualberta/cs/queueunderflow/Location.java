e0607e83b390e94aff6e6714ef66878dabc52b2b
capearso <capearso@ualberta.ca>
2014-11-14 23:05:45 -0700

    geolocation
    Added to android manifest to use location services. Started the location
    class. Need to get info on extent of location.

---
+++ QueueUnderflow/src/ca/ualberta/cs/queueunderflow/Location.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.queueunderflow;
+
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+
+//needs work but need to know if we need to use google play services or not and what extent of location services we need.
+public class Location {
+
+	/* Use the LocationManager class to obtain GPS locations */
+	//LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	//LocationListener locListener = new MyLocationListener();
+}
